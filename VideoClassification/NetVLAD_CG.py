44b4e37b58a6d7629fd9a23e5ffb5c5142f756d7
cfvedova <conorfvedova@google.com>
2020-05-28 22:47:24 +0000

    Change to imports

--- VideoClassification/NetVLAD_CG.py
+++ VideoClassification/NetVLAD_CG.py
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import tensorflow as tf
-import tensorflow.addons#.tensorflow_addons.layers.netvlad
+import tensorflow/addons#.tensorflow_addons.layers.netvlad
 import model_utils as utils
 
 class ContextGating(tf.keras.layers.Layer):
