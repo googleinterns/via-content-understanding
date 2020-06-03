020ebced7cd023656d3c94d81afcd3e97bfeb827
cfvedova <conorfvedova@google.com>
2020-06-03 15:46:28 +0000

    Fixing Model

--- VideoClassification/NetVLAD_CG.py
+++ VideoClassification/NetVLAD_CG.py
@@ -276,8 +276,9 @@ class VideoClassifier(tf.keras.Model):
 		fc_out = self.fc(vlad_out)
 		cg_out = self.first_cg(fc_out)
 		moe_out = self.moe(cg_out)
+		print(moe_out.shape)
 		final_out = self.second_cg(moe_out)
-
+		print(final_out.shape)
 		return final_out
 
 #Fix num_frames in call function
