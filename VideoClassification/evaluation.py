c56d2d83198d0701fda9b705cbd2921127fa8e68
cfvedova <conorfvedova@google.com>
2020-06-04 15:45:33 +0000

    Adding Evaluation

--- VideoClassification/evaluation.py
+++ VideoClassification/evaluation.py
@@ -5,6 +5,7 @@ import eval_util
 import NetVLAD_CG
 import reader_utils
 import readers
+import loss
 
 def test(model_dir, num_clusters=64, batch_size=64, iterations=None, random_frames=True, num_mixtures=2, fc_units=2048):
 	data_reader = reader_utils.get_reader()
@@ -12,7 +13,7 @@ def test(model_dir, num_clusters=64, batch_size=64, iterations=None, random_fram
 	test_dataset = data_reader.get_dataset('/home/conorfvedova_google_com/data/test/', batch_size=batch_size, num_workers=8, type="test")
 
 	numpy_dataset = tfds.as_numpy(test_dataset)
-	for ex in numpy_dataset:
+	
 		print(ex)
 		assert False
 	num_frames = data_reader.max_frames
@@ -24,14 +25,22 @@ def test(model_dir, num_clusters=64, batch_size=64, iterations=None, random_fram
 	model = NetVLAD_CG.VideoClassifier(num_clusters, video_input_shape, audio_input_shape, fc_units=fc_units, iterations=iterations, random_frames=random_frames, num_classes=data_reader.num_classes, num_mixtures=num_mixtures)
 	model.load_weights(model_dir)
 
-
-
-	predictions = model.evaluate(test_dataset)
-	predictions = tf.transpose(predictions)
-
 	evaluation_metrics = eval_util.EvaluationMetrics(data_reader.num_classes, 20)
+	for batch in numpy_dataset:
+		test_input = tf.convert_to_tensor(batch[0])
+		test_labels = tf.convert_to_tensor(batch[1])
 
-	evaluation_metrics.accumulate(predictions, )
+		predictions = model.evaluate(test_input)
+		predictions = tf.transpose(predictions)
+		print(predictions.shape)
+		
+		loss_vals = loss.eval_loss(test_labels, predictions)
+		print(loss_vals.shape)
+
+		evaluation_metrics.accumulate(predictions, test_labels, loss_vals)
+	eval_dict = evaluation_metrics.get()
+
+	print(eval_dict)
 
 if __name__ == "__main__":
 	test("/home/conorfvedova_google_com/saved_model/model-final.h5")
