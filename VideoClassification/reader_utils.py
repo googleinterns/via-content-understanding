c8bedd30d2d262f98156d70deef6150777eb296e
cfvedova <conorfvedova@google.com>
2020-06-09 23:19:59 +0000

    Fixing Training

--- VideoClassification/reader_utils.py
+++ VideoClassification/reader_utils.py
@@ -3,11 +3,11 @@ import tensorflow as tf
 from tensorflow.io import gfile
 
 
-def get_reader(num_samples, random_frames, feature_names='rgb,audio', feature_sizes='1024,128'):
+def get_reader(feature_names='rgb,audio', feature_sizes='1024,128'):
 	# Convert feature_names and feature_sizes to lists of values.
 	feature_names, feature_sizes = GetListOfFeatureNamesAndSizes(feature_names, feature_sizes)
 
-	reader = readers.YT8MFrameFeatureDataset(feature_names=feature_names, feature_sizes=feature_sizes, num_samples=num_samples, random_frames=random_frames)
+	reader = readers.YT8MFrameFeatureDataset(feature_names=feature_names, feature_sizes=feature_sizes)
 
 	return reader
 
