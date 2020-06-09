b8b88b8af79b13a7f95d82aba2e59d5e20c4f495
cfvedova <conorfvedova@google.com>
2020-06-09 16:59:29 +0000

    Testing Readers

--- VideoClassification/train.py
+++ VideoClassification/train.py
@@ -39,8 +39,9 @@ def train(epochs=50, lr=0.01, num_clusters=256, batch_size=64, random_frames=Tru
 
 	validation_dataset = data_reader.get_dataset('/home/conorfvedova_google_com/data/validate/', batch_size=batch_size, num_workers=8, type="validate")
 
-	iterator = train_dataset.make_one_shot_iterator()
-	print(iterator.get_next())
+	iterator = tfds.as_numpy(train_dataset)
+	for i in iterator:
+		print(i)
 
 	assert False
 
