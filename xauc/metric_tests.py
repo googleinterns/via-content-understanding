62ba86b61131edd2e2b5cf16af160a11adf63797
Ryan Ehrlich <Naqu6@users.noreply.github.com>
2020-06-10 12:05:40 -0700

    Implement xAUC metric	 (#2)
    Work done:
    
    Implemented xAUC metric
    Implemented balanced metrics xAUC0 and xAUC1
    Added unit tests for xAUC metrics and helper code

---
+++ xauc/metric_tests.py
@@ -0,0 +1,111 @@
+# Lint as: python3
+"""Unit tests for metrics.py
+
+    Copyright 2020 Google LLC
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+import unittest
+import metrics
+
+import numpy as np
+
+class TestCrossAUCMetrics(unittest.TestCase):
+    """Unit tests for the xAUC metrics in the metrics module.
+
+    Attrs:
+        maximum_error: the maximum allowable difference between the target_score
+            and the calculated score.
+    """
+
+    maximum_error = 1e-6
+
+    def verify_metrics_match(self, expected, calculated):
+        """Verify the calculated and expected metrics match."""
+
+        self.assertTrue(
+            abs(expected.xauc - calculated.xauc) < self.maximum_error)
+
+        self.assertTrue(
+            abs(expected.xauc0 - calculated.xauc0) < self.maximum_error)
+
+        self.assertTrue(
+            abs(expected.xauc1 - calculated.xauc1) < self.maximum_error)
+
+    def verify_metrics(
+        self, protected_positive_scores, protected_negative_scores,
+        expected_protected_metrics, other_positive_scores,
+        other_negative_scores, expected_other_metrics):
+        """Asserts that calculated metrics are equal to the expected metrics.
+
+        Note: this method assumes that for arg_a and arg_b, 1 is the desired 
+        label 0 is the undesired label.
+
+        Args:
+            metric: a function that takes in two inputs, arg_a and arg_b, and
+                returns a numerical score.
+            target_score: the expected numerical score of metric(arg_a, arg_b). 
+            arg_a: a vector that's the first parameter of metric.
+            arg_b: a vector that's the second parameter of metric.
+        """
+        protected_metrics, other_metrics = metrics.calculate_xauc_metrics(
+            protected_positive_scores, protected_negative_scores,
+            other_positive_scores, other_negative_scores, 1, 0)
+
+        self.verify_metrics_match(protected_metrics, expected_protected_metrics)
+        self.verify_metrics_match(other_metrics, expected_other_metrics)
+    
+    def test_biased_scores(self):
+        """Tests calculating xAUC scores for biased scores."""
+
+        protected_positive_scores = np.array([0.75, 0.64, 0.46])
+        protected_negative_scores = np.array([0.33, 0.21, 0.10])
+
+        other_positive_scores = np.array([0.83, 0.81, 0.61, 0.58])
+        other_negative_scores = np.array([0.74, 0.41, 0.49, 0.44])
+
+        expected_protected_metrics = metrics.xAUCMetrics(
+            xauc=9/12, xauc0=1, xauc1=18/21)
+
+        expected_other_metrics = metrics.xAUCMetrics(
+            xauc=1.0, xauc0=23/28, xauc1=26/28)
+
+        self.verify_metrics(protected_positive_scores,
+            protected_negative_scores, expected_protected_metrics,
+            other_positive_scores, other_negative_scores,
+            expected_other_metrics)
+
+    def test_fair_scores(self):
+        """Tests calculating xAUC scores for fair scores."""
+
+        protected_positive_scores = np.array([0.96, 0.8, 0.4])
+        protected_negative_scores = np.array([0.6, 0.33, 0.21])
+
+        other_positive_scores = np.array([0.83, 0.94, 0.3])
+        other_negative_scores = np.array([0.7, 0.3, 0.2])
+
+        expected_protected_metrics = metrics.xAUCMetrics(
+            xauc=8/9, xauc0=15/18, xauc1=16/18)
+
+        expected_other_metrics = metrics.xAUCMetrics(
+            xauc=7/9, xauc0=15/18, xauc1=14/18)
+
+        self.verify_metrics(protected_positive_scores,
+            protected_negative_scores, expected_protected_metrics,
+            other_positive_scores, other_negative_scores,
+            expected_other_metrics)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ xauc/probability_tests.py
@@ -0,0 +1,134 @@
+# Lint as: python3
+"""Tests the probability functions in probability.py.
+
+    Copyright 2020 Google LLC
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+import unittest
+import numpy as np
+import probability
+
+class ProbabilityFunctionsTest(unittest.TestCase):
+    """Unit tests for functions in probability.py.
+
+    Attrs:
+        maximum_error: upper bound on difference between expected and calculated
+            value.
+    """
+
+    maximum_error = 1e-6
+
+    calculator_preferred_positive = probability.ProbabilityCalculator(1, 0)
+    calculator_preferred_negative = probability.ProbabilityCalculator(0, 1)
+
+    def assert_probability_calculated_correctly(
+        self, preferred_scores, undesired_scores, expected_probability):
+        """Assert the calculated probability is equal to the expected value.
+
+        Note: this method assumes that for preferred_scores and
+        undesired_scores, 1 is the label for preferred outcomes and 0 is the
+        label for undesired outcomes.
+            preferred_scores: a vector of preferred_scores.
+            undesired_scores: a vector of undesired_scores.
+            expected_probability: the expected probability a score in 
+                preferred_scores is ranked more preferred than a score in 
+                undesired_scores.
+        """
+
+        expected_score_positive = (self.calculator_preferred_positive
+            .probability_preferred_ranked_above_undesired(
+                preferred_scores, undesired_scores))
+
+        self.assertTrue(
+            abs(expected_score_positive - expected_probability) < \
+            self.maximum_error)
+
+        expected_score_negative = (self.calculator_preferred_negative
+            .probability_preferred_ranked_above_undesired(
+                1 - preferred_scores, 1 - undesired_scores))
+
+        self.assertTrue(
+            abs(expected_score_negative - expected_probability) < \
+            self.maximum_error)
+
+    def test_probability_functions_unordered_different_sizes(self):
+        """Tests with unordered/differently sized input vectors."""
+        target_prob = 15/20
+
+        scores_preferred = np.array([0.7, 0.15, 0.5, 0.25, 0.3])
+        scores_undesired = np.array([0.2, 0.4, 0.2, 0.1])
+
+        self.assert_probability_calculated_correctly(
+            scores_preferred, scores_undesired, target_prob)
+
+    def test_probability_functions_ordered_same_size(self):
+        """Tests with ordered/same sized input vectors."""
+        target_prob = 11/16
+
+        scores_preferred = np.array([0.7, 0.65, 0.5, 0.05])
+        scores_undesired = np.array([0.6, 0.3, 0.2, 0.1])
+
+        self.assert_probability_calculated_correctly(
+            scores_preferred, scores_undesired, target_prob)
+
+
+    def test_high_probability(self):
+        """Tests calculating a probability with both label arrangements."""
+        target_prob = 15/16
+
+        scores_preferred = np.array([0.9, 0.8, 0.7, 0.4])
+        scores_undesired = np.array([0.5, 0.3, 0.2, 0.1])
+
+        self.assert_probability_calculated_correctly(
+            scores_preferred, scores_undesired, target_prob)
+
+    def test_low_probability(self):
+        """Tests calculating a probability with both label arrangements."""
+        target_prob = 7/16
+
+        scores_preferred = np.array([0.8, 0.7, 0.4, 0.2])
+        scores_undesired = np.array([0.9, 0.6, 0.5, 0.3])
+
+        self.assert_probability_calculated_correctly(
+            scores_preferred, scores_undesired, target_prob)
+
+    def test_same_values(self):
+        """Tests calculating an probabilites with identical scores."""
+        target_prob = 10/15
+
+        scores_preferred = np.array([0.8, 0.7, 0.6, 0.5, 0.4])
+        scores_undesired = np.array([0.6, 0.5, 0.35])
+
+        self.assert_probability_calculated_correctly(
+            scores_preferred, scores_undesired, target_prob)
+
+        self.assert_probability_calculated_correctly(
+            np.array([0.1]), np.array([0.1]), 0)
+
+    def test_edge_cases(self):
+        """Tests edge cases."""
+        self.assert_probability_calculated_correctly(
+            np.array([1.0]), np.array([0.0]), 1)
+
+        self.assert_probability_calculated_correctly(
+            np.array([0.0]), np.array([1.0]), 0)
+
+    def test_invalid_probabilites(self):
+        with self.assertRaises(ValueError):
+            (self.calculator_preferred_positive
+                .probability_preferred_ranked_above_undesired(
+                    np.array([]), np.array([])))
+
+if __name__ == "__main__":
+    unittest.main()
