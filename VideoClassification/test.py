cde7aca59cd5a81a0f2ae986644200c658cdbd05
cfvedova <conorfvedova@google.com>
2020-06-18 16:59:23 +0000

    Fixing test script

--- VideoClassification/test.py
+++ VideoClassification/test.py
@@ -66,9 +66,9 @@ def test_model(model, data_reader, test_dir, batch_size):
 		# eval_dict = {"AUCPR": auc_pr, "precision": precision, "recall": recall}
 
 		#print(eval_dict)
-		print(np.argmax(predictions, 1).shape)
+		print(np.argmax(predictions, 1))
 		num_true = np.array(test_labels)[np.arange(80),np.argmax(predictions, 1)]
-		print(num_true.shape)
+		print(num_true)
 		num_true = np.sum(num_true)
 		total_true += num_true
 		num_samples += 80
