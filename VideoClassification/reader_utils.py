bc4943feb5f6437940d5485680aea06a8297bc9f
cfvedova <conorfvedova@google.com>
2020-06-19 20:46:55 +0000

    Fixing line issues

--- VideoClassification/reader_utils.py
+++ VideoClassification/reader_utils.py
@@ -58,16 +58,11 @@ def _get_list_of_feature_names_and_sizes(feature_names, feature_sizes):
     List of the feature names and list of the dimensionality of each feature.
     Elements in the first/second list are strings/integers.
   """
-  list_of_feature_names = [
-      feature_names.strip() for feature_names in feature_names.split(",")
-  ]
-  list_of_feature_sizes = [
-      int(feature_sizes) for feature_sizes in feature_sizes.split(",")
-  ]
+  list_of_feature_names = [feature_names.strip() for feature_names in feature_names.split(",")]
+  list_of_feature_sizes = [int(feature_sizes) for feature_sizes in feature_sizes.split(",")]
+
   if len(list_of_feature_names) != len(list_of_feature_sizes):
-    logging.error("length of the feature names (=" +
-                  str(len(list_of_feature_names)) + ") != length of feature "
-                  "sizes (=" + str(len(list_of_feature_sizes)) + ")")
+    logging.error("length of the feature names (=" + str(len(list_of_feature_names)) + ") != length of feature sizes (=" + str(len(list_of_feature_sizes)) + ")")
 
   return list_of_feature_names, list_of_feature_sizes
 
