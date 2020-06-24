6bc5c49f274d741e728f87e53eaf0e20be299213
cfvedova <conorfvedova@google.com>
2020-06-24 19:32:36 +0000

    Fixed review issues

--- VideoClassification/loss_unittest.py
+++ VideoClassification/loss_unittest.py
@@ -7,7 +7,7 @@ class TestLoss(unittest.TestCase):
 
   def test_correct(self):
     y_actual = tf.convert_to_tensor([[1,0],[0,1], [1,1]])
-    y_predicted = tf.convert_to_tensor([[1,0],[0,1], [1,1]])
+    y_predicted = tf.convert_to_tensor([[1.0,0],[0,1], [1,1]])
 
     loss_out = loss.custom_crossentropy(y_actual, y_predicted)
     self.assertEqual(loss_out, tf.zeros(()))
@@ -28,7 +28,7 @@ class TestLoss(unittest.TestCase):
 
   def test_epsilon(self):
     y_actual = tf.convert_to_tensor([[1,0],[0,1], [1,1]])
-    y_predicted = tf.convert_to_tensor([[0,1],[1,0], [0,0]])
+    y_predicted = tf.convert_to_tensor([[0.0,1],[1,0], [0,0]])
 
     loss_out = loss.custom_crossentropy(y_actual, y_predicted, epsilon=1)
     self.assertEqual(loss_out, tf.zeros(()))
