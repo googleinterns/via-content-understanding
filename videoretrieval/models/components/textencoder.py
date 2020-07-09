f88dd30540f3df6041b300219af841bac6e010e8
Ryan Ehrlich <ryanehrlich@google.com>
2020-07-09 16:58:34 +0000

    Updated documentation for text encoder

--- videoretrieval/models/components/textencoder.py
+++ videoretrieval/models/components/textencoder.py
@@ -22,6 +22,18 @@ from models.layers import GatedEmbeddingModule, NetVLAD
 class TextEncoder(tf.keras.Model):
     """Implementation of Text Encoder.
 
+    This model takes in contextual embeddings from a language model and maps
+    them to a fixed length, sharded embedding that corresponds to embeddings
+    produced by the corresponding video encoder. This model should be trained
+    in concert with a video encoder.
+
+    The contextual embeddings are first aggregated using netvald to a fixed
+    length vector. Then, for each expert in the corresponding video encoder,
+    this fixed length vector is passed through a Gated Embedding Module to
+    produce a shard of an embedding. The fixed length vector is also passed
+    through a single dense layer to produce weights that specify the relative
+    importance of the shards of the embeddings.
+
     Attributes:
         num_of_experts: the number of experts used.
         num_netvlad_clusters: the number of clusters in the NetVLAD model.
@@ -46,15 +58,15 @@ class TextEncoder(tf.keras.Model):
 
         Parameters:
             num_of_experts: number of experts used in the video encoder.
-            num_netvlad_clusters: number of clusters
+            num_netvlad_clusters: number of clusters in NetVLAD.
+            ghost_clusters: number of ghost clusters in NetVLAD.
             language_model_dimensionality: last dimension of output of language
                 model.
             encoded_expert_dimensionality: the dimensionality video experts
-                embeddings are computed down to. Final output size is num of
-                experts * encoded_expert_dimensionality.
+                embeddings are computed down to.
             kernel_initializer: the strategy used to initialize the weights in
                 dense layer's kernel.
-            bias_initial: the strategy used to initalize the weights in dense
+            bias_initial: the strategy used to initialize the weights in dense
                 layers' biases.
         """
         super(TextEncoder, self).__init__()
@@ -101,11 +113,12 @@ class TextEncoder(tf.keras.Model):
         First, the text is aggregated using netvlad. These aggregated
         text embeddings are inputted to each gated embedding module to generate 
         the normalized embeddings. The aggregated text embeddings are also
-        inputted into the moe_dense layer to generate the mixture weights.
+        inputted into a dense layer to generate the mixture weights.
 
         Returns: a tuple of two elements. First, a list of embeddings for the
-        text captions. Second, a tensor of mixture weights for the embeddings.
-
+        text captions. Each element of this list is a tensor of shape batch size
+        x encoded expert dimensionality. Second, a tensor of mixture weights
+        for the embeddings of shape batch size x number of experts.
         """
 
         aggregated_embeddings = self.netvlad(input_)
