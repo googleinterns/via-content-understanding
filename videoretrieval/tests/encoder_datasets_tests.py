72914cb47f04f27ec634bcfcee120235eff5575e
Ryan Ehrlich <ryanehrlich@google.com>
2020-07-10 22:40:01 +0000

    Worked on tests and formatting

--- videoretrieval/tests/encoder_datasets_tests.py
+++ videoretrieval/tests/encoder_datasets_tests.py
@@ -1,13 +1,59 @@
+"""Unit tests for the encoder datasets module.
+
+Copyright 2020 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 
 import unittest
 from train import encoder_datasets
 import tensorflow as tf
 import numpy as np
+from base import BaseExpert, BaseLanguageModel
 
 MOCK_TENSOR_SHAPE = (2, 2) 
 MOCK_TENSOR_DATA = tf.zeros(MOCK_TENSOR_SHAPE)
 NUM_MOCK_VIDEOS = 5
 MOCK_CAPTIONS_PER_VIDEO = 20
+
+class MockExpert(BaseExpert):
+
+    def __init__(self, is_constant_length, max_frames, embedding_shape):
+        self.constant_length = is_constant_length
+        self.max_frames = max_frames
+        self._embedding_shape = embedding_shape
+
+    @property
+    def name(self):
+        raise NotImplementedError()
+
+    @property
+    def embedding_shape():
+        return self._embedding_shape
+
+class MockLanguageModel(BaseLanguageModel):
+    def __init__(self, contextual_embeddings_shape):
+        self.contextual_embeddings_shape = contextual_embeddings_shape
+
+    def forward(self, ids):
+        raise NotImplementedError()
+
+    def encode(self, text):
+        raise NotImplementedError()
+
+    @property
+    def batch_size(self):
+        raise NotImplementedError()
 
 def make_mock_video_ids(num_videos):
     video_ids = []
@@ -47,19 +93,17 @@ def make_mock_precomputed_features(video_ids):
         mock_precomputed_features_half_missing]
 
 class TestEncoderDatasetsFunctions(unittest.TestCase):
+    """"Tests for functions in the encoder datasets module."""
+    mock_video_ids = make_mock_video_ids(NUM_MOCK_VIDEOS)
+    mock_dataset = make_mock_id_embeddings_pair_dataset(
+        mock_video_ids, MOCK_TENSOR_SHAPE)
+    mock_precomputed_features = make_mock_precomputed_features(
+        mock_video_ids)
 
     def test_replacing_video_id_with_expert_features(self):
         """Tests the replace_video_id_with_expert_features_wrapper function."""
-        mock_video_ids = make_mock_video_ids(NUM_MOCK_VIDEOS)
-
-        mock_dataset = make_mock_id_embeddings_pair_dataset(
-            mock_video_ids, MOCK_TENSOR_SHAPE)
-
-        mock_precomputed_features = make_mock_precomputed_features(
-            mock_video_ids)
-
         map_fn = encoder_datasets.replace_video_id_with_expert_features_wrapper(
-            mock_precomputed_features)
+            self.mock_precomputed_features)
 
         output = list(iter(mock_dataset.map(map_fn)))
 
@@ -70,7 +114,7 @@ class TestEncoderDatasetsFunctions(unittest.TestCase):
                 expert_features, missing_modalities)):
 
                 expected_feature, expected_missing = \
-                    mock_precomputed_features[feature_index][video_id]
+                    self.mock_precomputed_features[feature_index][video_id]
                 
                 self.assertTrue(np.array_equal(
                     feature.numpy(), expected_feature.numpy()))
@@ -85,15 +129,11 @@ class TestEncoderDatasetsFunctions(unittest.TestCase):
 
     def test_sample_captions(self):
         """Tests the sample captions method of encoder datasets."""
-        mock_video_ids = make_mock_video_ids(NUM_MOCK_VIDEOS)
-
-        mock_dataset = make_mock_id_embeddings_pair_dataset(
-            mock_video_ids, MOCK_TENSOR_SHAPE, MOCK_CAPTIONS_PER_VIDEO)
-
         mock_dataset = list(iter(encoder_datasets.sample_captions(
-            mock_dataset, MOCK_CAPTIONS_PER_VIDEO)))
+            self.mock_dataset, MOCK_CAPTIONS_PER_VIDEO)))
 
-        for expected_value, dataset_value in zip(mock_video_ids, mock_dataset):
+        for expected_value, dataset_value in zip(
+            self.mock_video_ids, mock_dataset):
             video_id, _ = dataset_value
 
             self.assertEqual(expected_value, video_id)
