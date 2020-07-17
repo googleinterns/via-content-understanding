e00bdd0b1f020824d153b600646b30f8d0e85df0
Ryan Ehrlich <ryanehrlich@google.com>
2020-07-17 20:50:07 +0000

    Update loss tests

--- videoretrieval/tests/loss_tests.py
+++ videoretrieval/tests/loss_tests.py
@@ -25,69 +25,92 @@ class TestBidirectionalMaxMarginRankingLoss(unittest.TestCase):
 
     error = 1e-5
 
-    def test_perfect_embeddings(self):
-        """Tests a mini batch of perfect embeddings."""
-        mock_video_embeddings = tf.Variable([
+
+    def test_loss_calculation_with_different_embeddings(self):
+        """Tests computing loss for mini-batches of varying quality embeddings.
+
+        This tests computing loss for three sets of embeddings. The first set of
+        embeddings are perfect, the second are ok, and the third are bad.
+        """
+
+        mock_missing_experts = tf.constant([[False], [False], [False]])
+        mock_mixture_weights = tf.constant([[1.0], [1.0], [1.0]])
+
+        mock_perfect_video_embeddings = [tf.constant([
             [-1.0, 0.0, 0.0],
             [0.0, 0.0, -1.0],
             [0.0, 1.0, 0.0],
-        ])
+        ])]
 
-        mock_text_embeddings = tf.Variable([
+        mock_perfect_text_embeddings = [tf.constant([
             [-1.0, 0.0, 0.0],
             [0.0, 0.0, -1.0],
             [0.0, 1.0, 0.0],
-        ])
+        ])]
 
-        loss = bidirectional_max_margin_ranking_loss(mock_video_embeddings,
-            mock_text_embeddings, 1.0)
+        loss = bidirectional_max_margin_ranking_loss(
+            mock_perfect_video_embeddings,
+            mock_perfect_text_embeddings,
+            mock_mixture_weights,
+            mock_missing_experts,
+            1.0)
 
         self.assertTrue(abs(loss.numpy() - 0.0) < self.error)
 
-        loss = bidirectional_max_margin_ranking_loss(mock_video_embeddings,
-            mock_text_embeddings, 100.0)
+        loss = bidirectional_max_margin_ranking_loss(
+            mock_perfect_video_embeddings,
+            mock_perfect_text_embeddings,
+            mock_mixture_weights,
+            mock_missing_experts,
+            100.0)
 
-        self.assertTrue(abs(loss.numpy() - 132.0) < self.error)
+        self.assertTrue(abs(loss.numpy() - 99.0) < self.error)
 
-    def test_good_embeddings(self):
-        """Tests a minibatch of good embeddings."""
-        mock_video_embeddings = tf.Variable([
+        mock_good_video_embeddings = [tf.constant([
             [-0.9938837 ,  0.11043153],
             [-0.70710677,  0.70710677],
             [0.0, 1.0],
-        ])
+        ])]
 
-        mock_text_embeddings = tf.Variable([
+        mock_good_text_embeddings = [tf.constant([
             [-1.0, 0.0],
             [-0.5547002,  0.8320503],
             [0.0, 1.0],
-        ])
+        ])]
 
-        loss = bidirectional_max_margin_ranking_loss(mock_video_embeddings,
-            mock_text_embeddings, 1.0)
+        loss = bidirectional_max_margin_ranking_loss(
+            mock_good_video_embeddings,
+            mock_good_text_embeddings,
+            mock_mixture_weights,
+            mock_missing_experts,
+            1.0)
 
-        expected_value = 0.6779908
+        self.assertTrue(abs(loss.numpy() - 0.5084931) < self.error)
 
-        self.assertTrue(abs(loss.numpy() - 0.6779908) < self.error)
 
-    def test_bad_embeddings(self):
-        """Tests a mini batch of bad embeddings."""
+        mock_missing_experts = tf.constant([[False], [False], [False], [False]])
+        mock_mixture_weights = tf.constant([[1.0], [1.0], [1.0], [1.0]])
 
-        mock_video_embeddings = tf.Variable([
+        mock_bad_video_embeddings = [tf.constant([
             [0.25, 0.25],
             [1.0, 1.0],
             [0.6, 0.5],
             [0.9, 0.8],
-        ])
+        ])]
 
-        mock_text_embeddings = tf.Variable([
+        mock_bad_text_embeddings = [tf.constant([
             [-1.0, 0.0],
             [0, 1.0],
             [-1.0, 1.0],
             [0.7, 0.6],
-        ])
+        ])]
 
-        loss = bidirectional_max_margin_ranking_loss(mock_video_embeddings,
-            mock_text_embeddings, 1.5)
+        loss = bidirectional_max_margin_ranking_loss(
+            mock_bad_video_embeddings,
+            mock_bad_text_embeddings,
+            mock_mixture_weights,
+            mock_missing_experts,
+            1.5)
 
-        self.assertTrue(abs(loss.numpy() - 1.815000005) < self.error)
+        self.assertTrue(abs(loss.numpy() - 1.21000000333) < self.error)
+
