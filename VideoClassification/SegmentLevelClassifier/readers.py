3e3719d084cb8e559ca79e13c996a6592ebb70c9
cfvedova <conorfvedova@google.com>
2020-07-24 16:10:08 +0000

    Fixed candidate generation

--- VideoClassification/SegmentLevelClassifier/readers.py
+++ VideoClassification/SegmentLevelClassifier/readers.py
@@ -240,7 +240,7 @@ class PreprocessingDataset():
     context, features = tf.io.parse_single_sequence_example(serialized_example, context_features=context_features, sequence_features=sequence_features)
 
     video_id = context["id"]
-    print(self.candidates)
+    print(len(self.candidates))
     print(video_id)
     print(video_id.ref())
     if video_id.ref() in self.candidates.keys():
