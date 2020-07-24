5f8b4bb8c61c99375ca0f3a6a694dfb115e3e85e
cfvedova <conorfvedova@google.com>
2020-07-24 17:39:51 +0000

    Fixed candidate generation

--- VideoClassification/SegmentLevelClassifier/readers.py
+++ VideoClassification/SegmentLevelClassifier/readers.py
@@ -254,4 +254,4 @@ class PreprocessingDataset():
     # print(context)
     print(context)
     print(features)
-    return (context, features)#(context["id"], context["labels"], context["segment_labels"], context["segment_start_times"], context["segment_scores"], features["rgb"], features["audio"])
+    return (context)#(context["id"], context["labels"], context["segment_labels"], context["segment_start_times"], context["segment_scores"], features["rgb"], features["audio"])
