c4a19db65516a97ab81ab6b2e3dc20b4a87c5547
cfvedova <conorfvedova@google.com>
2020-07-27 13:44:29 +0000

    Fixing cand

--- VideoClassification/SegmentLevelClassifier/candidate_generation.py
+++ VideoClassification/SegmentLevelClassifier/candidate_generation.py
@@ -57,7 +57,7 @@ def serialize_video(context, features):
   rgb = tf.train.Feature(bytes_list=rgb)
   features = {"audio":tf.train.FeatureList(feature=[audio]), "rgb":tf.train.FeatureList(feature=[rgb])}
   features = tf.train.FeatureLists(feature_list=features)
-  print(context["segment_start_times"].values)
+  print(context)
   context = tf.train.FeatureLists(feature_list=context)
   example = tf.train.SequenceExample(feature_lists=features, context=context)
 
