bb0acd19bd5b10150fb9b86d05f48f1c0b78af69
cfvedova <conorfvedova@google.com>
2020-07-27 15:24:44 +0000

    Fixing cand

--- VideoClassification/SegmentLevelClassifier/candidate_generation.py
+++ VideoClassification/SegmentLevelClassifier/candidate_generation.py
@@ -186,4 +186,4 @@ if __name__ == "__main__":
 
   segment_reader = readers.PreprocessingDataset()
   input_dataset = segment_reader.get_dataset("/home/conorfvedova_google_com/data/segments/validation", batch_size=1, type="validate")
-  save_data("~/data/segments/candidate_validation", input_dataset, candidates)
+  save_data("/home/conorfvedova_google_com/data/segments/candidate_validation", input_dataset, candidates)
