e6eced10e297a856a5e78bab8199dc13ba9fb798
cfvedova <conorfvedova@google.com>
2020-07-27 15:47:16 +0000

    Fixing cand

--- VideoClassification/SegmentLevelClassifier/candidate_generation.py
+++ VideoClassification/SegmentLevelClassifier/candidate_generation.py
@@ -176,6 +176,7 @@ def generate_candidates(input_dataset, model, k, class_csv):
     video_id = tf.convert_to_tensor(video[0])[0].ref()
     video_input = tf.convert_to_tensor(video[1])
     print(video_input)
+    assert False
     probability_holder.add_data(video_id, model.predict(video_input)[0])
     video_num += 1
   return probability_holder.find_candidates()
