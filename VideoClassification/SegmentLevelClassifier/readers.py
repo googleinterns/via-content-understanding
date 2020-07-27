76d78ddb903d5544283043ce30435b33bb24c002
cfvedova <conorfvedova@google.com>
2020-07-27 15:35:13 +0000

    Fixing cand

--- VideoClassification/SegmentLevelClassifier/readers.py
+++ VideoClassification/SegmentLevelClassifier/readers.py
@@ -83,7 +83,8 @@ class VideoDataset():
     Returns:
       dataset: TFRecordDataset of the input training data
     """
-    files = tf.io.matching_files(os.path.join(data_dir, '%s0000.tfrecord' % type))
+    files = tf.io.matching_files(os.path.join(data_dir, '%s*.tfrecord' % type))
+
     
     files_dataset = tf.data.Dataset.from_tensor_slices(files)
     files_dataset = files_dataset.batch(tf.cast(tf.shape(files)[0], tf.int64))
