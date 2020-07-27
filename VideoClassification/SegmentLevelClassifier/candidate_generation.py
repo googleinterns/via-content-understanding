845715e84f1710cf0cb7865e45fcf3def8a6a667
cfvedova <conorfvedova@google.com>
2020-07-27 15:37:24 +0000

    Fixing cand

--- VideoClassification/SegmentLevelClassifier/candidate_generation.py
+++ VideoClassification/SegmentLevelClassifier/candidate_generation.py
@@ -181,7 +181,7 @@ if __name__ == "__main__":
   #do candidate gen and keep track of video_ids. Then, pass list of (video_id, class_list) pairs to dataset to then add them while loading data. 
   #then simply parse that like Ryan and write it in shards
   video_reader = readers.VideoDataset()
-  input_dataset = video_reader.get_dataset("/home/conorfvedova_google_com/data/segments/validation", batch_size=1, type="validate")
+  input_dataset = video_reader.get_dataset("/home/conorfvedova_google_com/data/segments/candidate_validation", batch_size=1, type="validate")
 
   model = load_model("../model_weights.h5")
 
