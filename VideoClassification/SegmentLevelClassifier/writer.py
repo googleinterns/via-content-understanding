5edb56c1ae67ce1865440744af79535effb1a492
cfvedova <conorfvedova@google.com>
2020-07-29 15:01:52 +0000

    Class Specific Features

--- VideoClassification/SegmentLevelClassifier/writer.py
+++ VideoClassification/SegmentLevelClassifier/writer.py
@@ -224,7 +224,7 @@ def split_data(data_dir, input_dataset, shard_size=85, num_classes=1000, file_ty
       print(new_context)
       print(new_features)
       label = new_context["segment_label"]
-      label = convert_labels([label]).numpy()
+      label = convert_labels([label]).numpy()[0]
       serialized_video = serialize_data(new_context, new_features, "segment")
       video_holder[label].append(serialized_video)
   
