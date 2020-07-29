8c887e5bb3adc7b617d8afd77cc205c90de2de89
cfvedova <conorfvedova@google.com>
2020-07-29 16:15:38 +0000

    Class Specific Features

--- VideoClassification/SegmentLevelClassifier/writer.py
+++ VideoClassification/SegmentLevelClassifier/writer.py
@@ -182,7 +182,6 @@ def save_data(new_data_dir, input_dataset, candidates, file_type="test", shard_s
     features = video[1]
     context = add_candidate_content(context, candidates)
     serialized_video = serialize_data(context, features, "video")
-    assert False
     shard.append(serialized_video)
     shard_counter += 1
     if shard_counter == shard_size:
