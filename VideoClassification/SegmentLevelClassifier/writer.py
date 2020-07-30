c5a19c3235b25e61a9e12a4fe9bf2bbd78528515
cfvedova <conorfvedova@google.com>
2020-07-30 13:12:38 +0000

    CSF

--- VideoClassification/SegmentLevelClassifier/writer.py
+++ VideoClassification/SegmentLevelClassifier/writer.py
@@ -250,9 +250,6 @@ def split_data(data_dir, input_dataset, shard_size=85, num_classes=1000, file_ty
 
         label = new_context["segment_label"]
         label = convert_labels(label).numpy()[0]
-        if label == 201:
-          print(new_context)
-          print(new_features)
         serialized_video = serialize_data(new_context, new_features, "segment")
         video_holder[label].append(serialized_video)
       else:
