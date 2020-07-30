7dd0206965944c1972ae27f1300816c2583110c2
cfvedova <conorfvedova@google.com>
2020-07-30 17:57:15 +0000

    CSF

--- VideoClassification/SegmentLevelClassifier/readers.py
+++ VideoClassification/SegmentLevelClassifier/readers.py
@@ -519,7 +519,7 @@ class InputDataset():
         feature_name: tf.io.FixedLenSequenceFeature([], dtype=tf.string)
         for feature_name in self.feature_names[:2]
     }
-    sequence_features[self.feature_names[-1]] = tf.io.FixedLenSequenceFeature([], dtype=tf.float32)
+    #sequence_features[self.feature_names[-1]] = tf.io.FixedLenSequenceFeature([], dtype=tf.float32)
     context, features = tf.io.parse_single_sequence_example(serialized_example, context_features=context_features, sequence_features=sequence_features)
     num_features = len(self.feature_names) - 1
 
