bc14c1803f0f149c391be4066f96c8cec5e0ff23
cfvedova <conorfvedova@google.com>
2020-07-30 19:04:20 +0000

    CSF

--- VideoClassification/SegmentLevelClassifier/train.py
+++ VideoClassification/SegmentLevelClassifier/train.py
@@ -32,6 +32,8 @@ def crossentropy(y_actual, y_predicted, epsilon=10e-6, alpha=0.5):
   """
   float_labels = tf.cast(y_actual, tf.float32)
 
+  print(float_labels)
+  print(y_predicted)
   cross_entropy_loss = 2*(alpha*float_labels * tf.math.log(y_predicted + epsilon) + (1-alpha)*(1 - float_labels) * tf.math.log(1 - y_predicted + epsilon))
   cross_entropy_loss = tf.math.negative(cross_entropy_loss)
 
