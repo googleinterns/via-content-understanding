311b52312ed4d47fedb8418c4930fb4e43202ec8
cfvedova <conorfvedova@google.com>
2020-08-04 17:02:39 +0000

    Updated files

--- VideoClassification/SegmentLevelClassifier/evaluate.py
+++ VideoClassification/SegmentLevelClassifier/evaluate.py
@@ -31,7 +31,7 @@ def evaluate_example(model, example, num_classes=1000):
   video_matrix = tf.convert_to_tensor(example[0])
   class_features_lists = tf.reshape(example[1].values, [-1, 1, 3])
   for class_features_list in class_features_lists:
-    prediction = model.predict((video_matrix, tf.convert_to_tensor(class_features_list[0][0])))
+    prediction = model.predict((video_matrix, tf.convert_to_tensor(class_features_list)[0][0]))
     class_num = tf.cast(class_features_list[0][0], tf.int64).numpy()
     if class_num <= 100:
       predictions[class_num] = prediction[0][0]
