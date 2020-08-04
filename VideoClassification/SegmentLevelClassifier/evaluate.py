cf7b2ff268a8e55666ea1d732a0fb8c98529d721
cfvedova <conorfvedova@google.com>
2020-08-04 22:02:38 +0000

    Updated files

--- VideoClassification/SegmentLevelClassifier/evaluate.py
+++ VideoClassification/SegmentLevelClassifier/evaluate.py
@@ -31,10 +31,9 @@ def evaluate_example(model, example, num_classes=1000):
   video_matrix = tf.convert_to_tensor(example[0])
   class_features_lists = tf.reshape(example[1].values, [-1, 1, 3])
   for class_features_list in class_features_lists:
-    prediction = model.predict((video_matrix, tf.convert_to_tensor([class_features_list[0][0]])))
+    prediction = model.predict((video_matrix, class_features_list))
     class_num = tf.cast(class_features_list[0][0], tf.int64).numpy()
-    if class_num <= 100:
-      predictions[class_num] = prediction[0][0]
+    predictions[class_num] = prediction[0][0]
   return tf.reshape(tf.convert_to_tensor(predictions), [1,-1])
 
 def evaluate_model(model, dataset):
@@ -51,15 +50,14 @@ def evaluate_model(model, dataset):
   pr_calculator = metrics.PrecisionAtRecall(0.7)
   rp_calculator = metrics.RecallAtPrecision(0.7)
   for input_data, label in dataset:
-    if np.where(label == 1)[1][0] <= 100:
-      prediction = evaluate_example(model, input_data)
-      #Update Metrics
-      aucroc_calculator.update_state(label, prediction)
-      aucpr_calculator.update_state(label, prediction)
-      pr_calculator.update_state(label, prediction)
-      rp_calculator.update_state(label, prediction)
-      print(f"Processing segment number {segment_num}")
-      segment_num += 1
+    prediction = evaluate_example(model, input_data)
+    #Update Metrics
+    aucroc_calculator.update_state(label, prediction)
+    aucpr_calculator.update_state(label, prediction)
+    pr_calculator.update_state(label, prediction)
+    rp_calculator.update_state(label, prediction)
+    print(f"Processing segment number {segment_num}")
+    segment_num += 1
   #Get results
   auc_roc = aucroc_calculator.result()
   auc_pr = aucpr_calculator.result()
