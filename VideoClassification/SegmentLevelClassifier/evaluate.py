bfdedcc4dbc693c58d2897c45c64c0852d3a6ad7
cfvedova <conorfvedova@google.com>
2020-08-04 02:41:38 +0000

    Updated files

--- VideoClassification/SegmentLevelClassifier/evaluate.py
+++ VideoClassification/SegmentLevelClassifier/evaluate.py
@@ -33,6 +33,8 @@ def evaluate_example(model, example, num_classes=1000):
     prediction = model.predict((video_matrix, class_features_list))
     class_num = tf.cast(class_features_list[0][0], tf.int64).numpy()
     predictions[class_num] = prediction
+  print(prediction)
+  assert False
   return predictions
 
 def evaluate_model(model, dataset):
@@ -55,10 +57,10 @@ def evaluate_model(model, dataset):
     print(label)
     prediction = evaluate_example(model, input_data)
     #Update Metrics
-    aucroc_calculator.update_state(label, predictions)
-    aucpr_calculator.update_state(label, predictions)
-    pr_calculator.update_state(label, predictions)
-    rp_calculator.update_state(label, predictions)
+    aucroc_calculator.update_state(label, prediction)
+    aucpr_calculator.update_state(label, prediction)
+    pr_calculator.update_state(label, prediction)
+    rp_calculator.update_state(label, prediction)
   #Get results
   auc_roc = aucroc_calculator.result()
   auc_pr = aucpr_calculator.result()
