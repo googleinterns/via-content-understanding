85f1a05df1f4410de892c32b679f900a1a581cb4
cfvedova <conorfvedova@google.com>
2020-08-07 20:07:12 +0000

    Fixing changes

--- VideoClassification/SegmentLevelClassifier/combine_dataset.py
+++ VideoClassification/SegmentLevelClassifier/combine_dataset.py
@@ -30,9 +30,7 @@ def combine_data(data_dir, input_dir, shard_size=85, file_type="test"):
   feature_storage = {}
   input_dataset_reader = readers.CombineSegmentDataset()
   input_dataset = input_dataset_reader.get_dataset(input_dir, batch_size=1)
-  segment_num = 0
   for segment in input_dataset:
-    print(segment_num)
     context = segment[0]
     features = segment[1]
     video_id = tf.convert_to_tensor(context["id"])[0].numpy()
